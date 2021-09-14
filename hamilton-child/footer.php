52159d7be8340700aeebbe96e124c6c4e72815f0
Colin Lewis <hello@colinlewis.se>
2021-09-14 06:52:05 +0100

    copyright link

--- hamilton-child/footer.php
+++ hamilton-child/footer.php
@@ -2,7 +2,7 @@
 
 		<footer class="site-footer section-inner">
 
-			<p class="copyright">&copy; <?php echo date('Y'); ?> <a href="/" class="site-name">Colin Lewis</a></p>
+			<p class="copyright">&copy; <?php echo date('Y'); ?> <a href="https://colinlewis.se" class="site-name">Colin Lewis</a></p>
 
 		</footer><!-- footer -->
 
