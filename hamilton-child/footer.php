a3786050fef0b0925af4990b71fd4f6e3549a8ce
Colin Lewis <hello@colinlewis.se>
2021-09-14 06:00:56 +0100

    init

---
+++ hamilton-child/footer.php
@@ -0,0 +1,13 @@
+		</main>
+
+		<footer class="site-footer section-inner">
+
+			<p class="copyright">&copy; <?php echo date('Y'); ?> <a href="<?php echo esc_url(home_url()); ?>" class="site-name"><?php bloginfo('name'); ?></a></p>
+
+		</footer><!-- footer -->
+
+		<?php wp_footer(); ?>
+
+		</body>
+
+		</html>
